c57b21ac9ce60c3b67e9e19c57e3777760ea3589
master16mh <master16.mh@gmail.com>
2025-11-04 14:52:05 -0300

    ave q rompio matia

--- DTO/DTOs-Recursos/RecursosPagPrincipalDTO.cs
+++ DTO/DTOs-Recursos/RecursosPagPrincipalDTO.cs
@@ -3,12 +3,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace DTO.DTOs_MaterialesYmaquinarias
 {
     public class RecursosPagPrincipalDTO
     {
+        [JsonPropertyName("id")]
+        public int RecursosId { get; set; }
         public string CodigoISO { get; set; }
         public string Nombre { get; set; }
         public string UnidadMedida { get; set; }
