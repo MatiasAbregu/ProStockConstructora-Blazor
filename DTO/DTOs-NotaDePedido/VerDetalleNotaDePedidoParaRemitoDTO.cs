5f30936c4064fbff2db8f6397f8d81bb417dfb99
MatiasAbregu <matiasabregu506@gmail.com>
2025-12-02 18:05:05 -0300

    Nota de pedido para remito

---
+++ DTO/DTOs-NotaDePedido/VerDetalleNotaDePedidoParaRemitoDTO.cs
@@ -0,0 +1,17 @@
+using DTO.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.DTOs_NotaDePedido
+{
+    public class VerDetalleNotaDePedidoParaRemitoDTO
+    {
+        public long Id { get; set; }
+        public long RecursoId { get; set; }
+        public string Recurso { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
